c4a6e2f0d6c28e20fa8c0820425c2c0a75e7bb7c
Cybereasy <118338725+cybereasy@users.noreply.github.com>
2024-02-15 23:13:46 +0530

    Add files via upload

---
+++ infoNum.py
@@ -0,0 +1,30 @@
+'''
+print("Created by CyberEasy")
+import phonenumbers 
+from phonenumbers import timezone,geocoder, carrier 
+num_1 = input("Enter the country code:")
+num_2 = input("Enter the Number: ")
+len1 = len(num_1)
+len2 = len(num_2)
+if len1 == 3:
+    print("country code Done...")
+else:
+    print( "Try again...")
+    exit()
+if len2 == 10:
+    print("country number Done...")
+else:
+    print(" Try again...")
+    exit()
+num_3 = str(num_1+num_2)
+phoneNumber = phonenumbers.parse(num_3)
+timeZone = timezone.time_zones_for_number(phoneNumber)
+Carrier = carrier.name_for_number(phoneNumber, 'en')
+Region = geocoder.description_for_number(phoneNumber, 'en')
+print (num_3)
+print(Carrier) 
+print(Region) 
+print(timeZone) 
+'''
+import phonenumbers
+print(dir())
